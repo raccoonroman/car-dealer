54de58ebc569dd957c5d6695ab263daa00bada35
Роман <kromanjs@gmail.com>
2019-01-31 17:26:32 +0200

    add new slider

--- source/js/script.js
+++ source/js/script.js
@@ -1,17 +1,31 @@
 "use strict";
 
-function openTestDrive(evt, cityName) {
+function openTestDrive(evt, tabName) {
 	var i, x, tablinks;
 	x = document.getElementsByClassName("test-drive__content");
 	for (i = 0; i < x.length; i++) {
 		x[i].style.display = "none";
 	}
-	tablinks = document.getElementsByClassName("tablink");
+	tablinks = document.getElementsByClassName("tablink-test-drive");
 	for (i = 0; i < x.length; i++) {
 		tablinks[i].className = tablinks[i].className.replace(" test-drive__tab--active", "");
 	}
-	document.getElementById(cityName).style.display = "block";
+	document.getElementById(tabName).style.display = "block";
 	evt.currentTarget.className += " test-drive__tab--active";
+}
+
+function openSevice(evt, tabName) {
+	var i, x, tablinks;
+	x = document.getElementsByClassName("service__content");
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tablink-service");
+	for (i = 0; i < x.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" service__tab--active", "");
+	}
+	document.getElementById(tabName).style.display = "block";
+	evt.currentTarget.className += " service__tab--active";
 }
 
 
@@ -32,9 +46,34 @@ $(document).ready(function(){
 });
 
 
+$(document).ready(function(){
+  $('.service__slides').slick({
+		dots: true,
+		// adaptiveHeight: true,
+		draggable: false,
+		swipe: false,
+		infinite:false,
+		appendArrows: ".service__arrows",
+		prevArrow: '<button type="button" class="slick-prev">Önceki Adım</button>',
+		nextArrow: '<button type="button" class="slick-next">Sonraki Adım</button>',
+		appendDots: ".service__dots",
+		dotsClass: "service__dots-list"
+	});
+});
+
+
 jQuery.datetimepicker.setLocale('tr');
 
-jQuery('#datetimepicker').datetimepicker({
+jQuery('#test-drive-datetimepicker').datetimepicker({
+	inline:true,
+	minDate:'-1970/01/02',
+	allowTimes:[
+  '09:00', '09:30', '10:30', '11:00', '11:30', '13:30', '14:30', '15:00', '15:30', '16:30', '17:00'
+ ]
+});
+
+
+jQuery('#service-datetimepicker').datetimepicker({
 	inline:true,
 	minDate:'-1970/01/02',
 	allowTimes:[
